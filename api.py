6f45885c400367b89026c8de68646f3cf930571e
Toktar <kostanayecosystem@gmail.com>
2024-04-19 08:48:37 +0000

    added prod mode and gunicorn support

--- api.py
+++ api.py
@@ -10,12 +10,41 @@ from handlers import auth_handler_v1
 from middlewares import auth_middleware_v1
 from middlewares import cors_middleware_v1
 
-config = configs['dev']
-routes = web.RouteTableDef()
-app = web.Application()
-db = Driver(config)
 
+async def app_factory():
+	config = configs['prod']
+	db = Driver(config)
+
+	routes = web.RouteTableDef()
+	app = web.Application()
+
+	app['db'] = db
+	app['config'] = config
+
+	# API versioning
+	APIv1 = web.Application()
+	APIv1.router.add_get('/auth', auth_handler_v1)
+	APIv1.router.add_view('/ads_items', AdsItemsV1)
+	APIv1.router.add_view('/ads_categories', AdsCategoriesV1)
+	APIv1.router.add_view('/user_mark_ad', UserMarkAdV1)
+	APIv1.router.add_view('/ads_validation', AdsValidationV1)
+	app.add_subapp('/api/rest/v1', APIv1)
+
+	# cors initialization
+	app.middlewares.append(cors_middleware_v1)
+	app.middlewares.append(auth_middleware_v1)
+
+	return app
+
+
+"""
 if __name__=='__main__':
+	config = configs['dev']
+	db = Driver(config)
+
+	routes = web.RouteTableDef()
+	app = web.Application()
+
 	app['db'] = db
 	app['config'] = config
 
@@ -34,3 +63,4 @@ if __name__=='__main__':
 
 	if config.CONFIG_NAME == 'dev':
 		web.run_app(app, port=config.PORT)
+"""
